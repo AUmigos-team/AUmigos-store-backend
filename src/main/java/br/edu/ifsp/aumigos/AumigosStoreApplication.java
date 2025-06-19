707777bfa43ef2cf87fba69fd6b17e7007e3a2b3
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2025-06-19 20:21:23 -0300

    initialize project structure

---
+++ src/main/java/br/edu/ifsp/aumigos/AumigosStoreApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aumigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AumigosStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AumigosStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsp/aumigos/AumigosStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aumigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AumigosStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
