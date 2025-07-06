b3794f111e5d733088a336b92ffaee3cb260a2d4
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2025-07-05 23:56:08 -0300

    refactor schema to use lower case

--- db/schema.sql
+++ db/schema.sql
@@ -2,56 +2,56 @@ drop database if exists aumigos_store;
 create database if not exists aumigos_store;
 use aumigos_store;
 
-drop table if exists CartItem;
-drop table if exists Cart;
-drop table if exists OrderItem;
-drop table if exists Review;
-drop table if exists CustomerOrder;
-drop table if exists Stock;
-drop table if exists Product;
-drop table if exists Brand;
-drop table if exists Subcategory;
-drop table if exists Category;
-drop table if exists Client;
-drop table if exists Status;
-drop table if exists PaymentMethod;
+drop table if exists cart_item;
+drop table if exists cart;
+drop table if exists order_item;
+drop table if exists review;
+drop table if exists customer_order;
+drop table if exists stock;
+drop table if exists product;
+drop table if exists brand;
+drop table if exists subcategory;
+drop table if exists category;
+drop table if exists client;
+drop table if exists status;
+drop table if exists payment_method;
 
-create table Category (
+create table category (
     id int primary key auto_increment,
     name varchar(100) not null
 );
 
-create table Subcategory (
-    id         int primary key auto_increment,
-    name       varchar(100) not null,
-    categoryId int,
-    foreign key (categoryId) references Category(id)
+create table subcategory (
+    id int primary key auto_increment,
+    name varchar(100) not null,
+    category_id int,
+    foreign key (category_id) references category(id)
 );
 
-create table Brand (
+create table brand (
     id int primary key auto_increment,
     name varchar(100) not null
 );
 
-create table Product (
+create table product (
     id int primary key auto_increment,
     name varchar(100) not null,
     description text,
     price decimal(10,2) not null,
-    subcategoryId int,
-    brandId int,
-    foreign key (subcategoryId) references Subcategory(id),
-    foreign key (brandId) references Brand(id)
+    subcategory_id int,
+    brand_id int,
+    foreign key (subcategory_id) references subcategory(id),
+    foreign key (brand_id) references brand(id)
 );
 
-create table Stock (
+create table stock (
     id int primary key auto_increment,
     quantity int not null,
-    productId int,
-    foreign key (productId) references Product(id)
+    product_id int,
+    foreign key (product_id) references product(id)
 );
 
-create table Client (
+create table client (
     id int primary key auto_increment,
     name varchar(100) not null,
     email varchar(150) not null,
@@ -59,62 +59,62 @@ create table Client (
     address text
 );
 
-create table PaymentMethod (
+create table payment_method (
     id int primary key auto_increment,
     name varchar(100) not null,
     description text
 );
 
-create table Status (
+create table status (
     id int primary key auto_increment,
     name varchar(50) not null,
     description text
 );
 
-create table CustomerOrder (
+create table customer_order (
     id int primary key auto_increment,
-    clientId int,
+    client_id int,
     date date not null,
-    statusId int,
-    paymentMethodId int,
-    totalValue decimal(10,2) not null,
-    foreign key (clientId) references Client(id),
-    foreign key (statusId) references Status(id),
-    foreign key (paymentMethodId) references PaymentMethod(id)
+    status_id int,
+    payment_method_id int,
+    total_value decimal(10,2) not null,
+    foreign key (client_id) references client(id),
+    foreign key (status_id) references status(id),
+    foreign key (payment_method_id) references payment_method(id)
 );
 
-create table OrderItem (
-    productId int,
-    orderId int,
+create table order_item (
+    product_id int,
+    order_id int,
     quantity int not null,
-    priceUnit decimal(10,2) not null,
-    primary key (productId, orderId),
-    foreign key (productId) references Product(id),
-    foreign key (orderId) references CustomerOrder(id)
+    price_unit decimal(10,2) not null,
+    primary key (product_id, order_id),
+    foreign key (product_id) references product(id),
+    foreign key (order_id) references customer_order(id)
 );
 
-create table Review (
+create table review (
     id int primary key auto_increment,
-    productId int,
-    clientId int,
+    product_id int,
+    client_id int,
     grade int,
     comment text,
     date date,
-    foreign key (productId) references Product(id),
-    foreign key (clientId) references Client(id)
+    foreign key (product_id) references product(id),
+    foreign key (client_id) references client(id)
 );
 
-create table Cart (
+create table cart (
     id int primary key auto_increment,
-    clientId int,
-    foreign key (clientId) references Client(id)
+    client_id int,
+    foreign key (client_id) references client(id)
 );
 
-create table CartItem (
+create table cart_item (
     id int primary key auto_increment,
-    cartId int not null,
-    productId int not null,
+    cart_id int not null,
+    product_id int not null,
     quantity int not null,
-    foreign key (cartId) references Cart(id),
-    foreign key (productId) references Product(id)
+    foreign key (cart_id) references cart(id),
+    foreign key (product_id) references product(id)
 );
