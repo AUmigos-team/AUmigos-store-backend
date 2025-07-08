4e698590f6a47829efdbb3a24b37cd751c34846d
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2025-07-08 16:07:27 -0300

    feat: add Cart and Order repositories; implement custom queries for client and stock checks

--- src/main/java/br/edu/ifsp/aumigos/repository/cart/CartRepository.java
+++ src/main/java/br/edu/ifsp/aumigos/repository/cart/CartRepository.java
@@ -1,7 +1,14 @@
 package br.edu.ifsp.aumigos.repository.cart;
 
 import br.edu.ifsp.aumigos.model.cart.Cart;
+import br.edu.ifsp.aumigos.model.cart.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+    @Query("select c from Cart c where c.client.id = :clientId")
+    Cart findByClientId(Integer clientId);
 }
