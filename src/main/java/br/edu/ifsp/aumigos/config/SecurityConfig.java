b8ba07373405d594d05563931d08fded10a1d5f4
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2025-07-11 03:12:46 -0300

    feat: update security configuration to require authentication for client API endpoints

--- src/main/java/br/edu/ifsp/aumigos/config/SecurityConfig.java
+++ src/main/java/br/edu/ifsp/aumigos/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/cart/**", "/api/order/**").authenticated()
+                        .requestMatchers("/api/cart/**", "/api/order/**", "/api/client/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
