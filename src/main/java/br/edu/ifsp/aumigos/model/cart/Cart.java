8577cba2d9569363d625130d9757e306711d577f
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2025-07-11 01:40:42 -0300

    refactor: update Cart entity to include cascade and orphan removal for CartItems

--- src/main/java/br/edu/ifsp/aumigos/model/cart/Cart.java
+++ src/main/java/br/edu/ifsp/aumigos/model/cart/Cart.java
@@ -3,6 +3,7 @@ package br.edu.ifsp.aumigos.model.cart;
 import br.edu.ifsp.aumigos.model.client.Client;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,7 +27,7 @@ public class Cart {
     @JsonIgnore
     private Client client;
 
-    @OneToMany(mappedBy = "cart")
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<CartItem> items;
 }
