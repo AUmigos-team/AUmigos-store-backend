1cb4d9ef88c4f4a4285fa7dc6dc7bc8053716736
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2025-07-11 05:49:45 -0300

    feat: add custom query method to OrderRepository for retrieving orders by client ID

--- src/main/java/br/edu/ifsp/aumigos/repository/order/OrderRepository.java
+++ src/main/java/br/edu/ifsp/aumigos/repository/order/OrderRepository.java
@@ -2,6 +2,12 @@ package br.edu.ifsp.aumigos.repository.order;
 
 import br.edu.ifsp.aumigos.model.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Query("select o from Order o where o.client.id = :clientId")
+    List<Order> findByClientId(Integer clientId);
 }
